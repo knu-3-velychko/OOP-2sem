add5bd7d4aa8087ea5bb9867d9972eef06462c79
Tai <justataya@gmail.com>
2020-06-13 06:58:33 +0300

    Complete SkipList

--- Tasks/Task5/src/main/java/com/knu/task5/Main.java
+++ Tasks/Task5/src/main/java/com/knu/task5/Main.java
@@ -1,7 +1,63 @@
 package com.knu.task5;
 
+import java.util.AbstractSet;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("It works!");
+    public static void main(String[] args) throws InterruptedException {
+        FineGrainedSkipList list = new FineGrainedSkipList(10);
+        System.out.println("Start execution flow");
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i] = new Thread(new MyProducer(i * 10, i * 10 + 10, list));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i].join();
+        }
+        System.out.println(list.toString());
+
+        for (int i = threads.length - 1; i >= 0; --i) {
+            threads[i] = new Thread(new MyConsumer(i * 10, i * 10 + 10, list));
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i].join();
+        }
+        System.out.println(list.toString());
+    }
+}
+
+class MyProducer implements Runnable {
+    final int start, end;
+    AbstractSet list;
+
+    MyProducer(int start, int end, AbstractSet list) {
+        this.start = start;
+        this.end = end;
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end; ++i)
+            list.add(i);
+    }
+}
+
+class MyConsumer implements Runnable {
+    final int start, end;
+    AbstractSet list;
+
+    MyConsumer(int start, int end, AbstractSet list) {
+        this.start = start;
+        this.end = end;
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end; ++i)
+            list.remove(i);
     }
 }
